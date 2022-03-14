8bc537fac2533a108d18073a4a67298675197583
schroderfernando16 <100815815+schroderfernando16@users.noreply.github.com>
2022-03-13 21:01:38 -0300

    Add files via upload

---
+++ SMA_Handshake.py
@@ -0,0 +1,14 @@
+import pandas
+import os
+import time
+
+class Cliente:
+    #funcao de cadastro do cliente 
+    #funcao pedido
+    #função acompanhamento
+
+class Produto:
+ #FUNCAO CADASTRO
+ #FUNCAO ESTOQUE
+ #FUNCAO ARMAZENAR SAFRA 
+ #FUNCAO DESPACHAR PEDIDO
