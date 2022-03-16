880ee609b9d278959d215ec44ab88152ca3a908e
Vinicius Espeçoto <especoto.vini@gmail.com>
2022-03-16 08:50:35 -0300

    Criando 'olá mundo' com Flask

--- SMA_Handshake.py
+++ SMA_Handshake.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import pandas
+from flask import Flask
 
 class Cliente:
     #funcao de cadastro do cliente 
@@ -13,4 +14,11 @@ class Produto:
  #FUNCAO ESTOQUE
  #FUNCAO ARMAZENAR SAFRA 
  #FUNCAO DESPACHAR PEDIDO
- 
+
+app = Flask(__name__)
+
+@app.route("/olamundo", methods=["GET"])
+def ola_mundo():
+    return {"key": "ola mundo"}
+
+app.run()
