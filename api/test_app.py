dc9d539251e4309373591dbca804f76702cb1ffe
Vinicius Espeçoto <especoto.vini@gmail.com>
2022-04-25 21:16:59 -0300

    Primeiro teste backend

--- api/test_app.py
+++ api/test_app.py
@@ -1,3 +1,27 @@
-def test_main():
-    x = 1
-    assert x == 1
+
+from api.models.db_models import Usuario
+from api.controllers import users_controller
+
+from mock import patch
+
+@patch('api.controllers.users_controller.add_usuario', return_value=1)
+def test_main(mock_test):
+
+    x = Usuario(
+        id = 1,
+        nome = 'teste',
+        telefone = '999999999',
+        email = 'teste@teste.com',
+        logradouro = 'rua teste',
+        numero = 123,
+        cep = '99999999',
+        cidade = 'sao paulo',
+        estado = 'sp',
+        cpf_cnpj = '11111111111',
+        tipo = 'consumidor',
+        senha = '1234'
+    )
+
+    y = users_controller.add_usuario(x)
+
+    mock_test.assert_called_once()
